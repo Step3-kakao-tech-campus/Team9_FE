c0a2da716fd1b0dd7e636b64de8d48529bcaf14c
dabinchiii <dabinchi388@naver.com>
2023-09-26 19:36:36 +0900

    Add main page

--- link-namu/src/App.js
+++ link-namu/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+// layouts
+import MainLayout from "./layouts/MainLayout";
+
+// pages
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<MainPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
