14e2d20df8587709a7c6ca35705b33436aae5a4a
dabinchiii <dabinchi388@naver.com>
2023-09-29 05:49:15 +0900

    refactor: Refactor sidebar component

---
+++ link-namu/src/components/atoms/SidebarTile.jsx
@@ -0,0 +1,13 @@
+const SidebarTile = ({ className, src, alt, padding = true }) => {
+  return (
+    <div
+      className={`${
+        padding && "p-[10px]"
+      } w-[60px] h-[60px] border-t border-[#c6c6c6] cursor-pointer`}
+    >
+      <img className="w-full h-full" src={src} alt={alt} />
+    </div>
+  );
+};
+
+export default SidebarTile;
