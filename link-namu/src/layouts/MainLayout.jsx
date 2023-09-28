f928b0e786ec195254de2f383a6f87ce6b53174d
dabinchiii <dabinchi388@naver.com>
2023-09-29 05:24:20 +0900

    feat: Add side menu bar

--- link-namu/src/layouts/MainLayout.jsx
+++ link-namu/src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/atoms/Sidebar";
+import Menubar from "../components/molecules/Menubar";
 
 const MainLayout = () => {
   return (
     <>
       <Sidebar />
+      <Menubar />
       <Outlet />
     </>
   );
