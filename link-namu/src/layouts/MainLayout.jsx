cf7bafce371bacc842f8e23e2d9ff1e11e79ff76
dabinchiii <dabinchi388@naver.com>
2023-10-08 23:03:25 +0900

    feat: Add close menu button

--- link-namu/src/layouts/MainLayout.jsx
+++ link-namu/src/layouts/MainLayout.jsx
@@ -5,6 +5,7 @@ import Sidebar from "../components/atoms/Sidebar";
 import Menubar from "../components/molecules/Menubar";
 
 import dehaze from "../assets/dehaze.png";
+import close from "../assets/x.png";
 
 const MainLayout = () => {
   const [open, setOpen] = useState(false);
@@ -18,10 +19,12 @@ const MainLayout = () => {
         <Sidebar />
       </div>
       {/* Menu */}
-      <div className="w-[60px] h-[60px] m-[10px] fixed left-0 top-0 rounded-[60px] shadow-md cursor-pointer"
+      <div
+        className="w-[60px] h-[60px] m-[10px] fixed left-0 top-0 rounded-[60px] shadow-md cursor-pointer bg-white hover:bg-[#d9d9d9] duration-300"
         onClick={() => {
           setOpen(true);
-        }}>
+        }}
+      >
         <button className="w-[40px] h-[40px] m-[10px]">
           <img src={dehaze} alt="open menubar" className="w-full h-full" />
         </button>
@@ -36,6 +39,14 @@ const MainLayout = () => {
             }}
           ></div>
           <Menubar />
+          <div
+            className="fixed top-0 left-[223px] w-[15px] h-[15px]"
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            <img src={close} alt="close menu" className="m-2" />
+          </div>
         </>
       )}
     </>
