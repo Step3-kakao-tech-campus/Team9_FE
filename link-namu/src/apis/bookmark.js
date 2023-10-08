14af29c828f136c91264cf54da0d02bc2b5dd0b1
dabinchiii <dabinchi388@naver.com>
2023-10-08 19:07:49 +0900

    feat: Add bookmark API functions

---
+++ link-namu/src/apis/bookmark.js
@@ -0,0 +1,66 @@
+import { instance } from "./api";
+
+/**
+ * 북마크 생성
+ * @param {string, string, string, number, string, array} param0
+ */
+export const createBookmark = ({
+  bookmarkName,
+  bookmarkLink,
+  bookmarkDescription = "",
+  categoryId,
+  imageUrl = "",
+  tags = [],
+}) => {
+  return instance.post("/api/bookmark/create", {
+    bookmarkName: bookmarkName,
+    bookmarkLink: bookmarkLink,
+    bookmarkDescription: bookmarkDescription,
+    categoryId: categoryId,
+    imageUrl: imageUrl,
+    tags: tags,
+  });
+};
+
+/**
+ * 북마크 삭제
+ * @param {number} param0
+ * @returns
+ */
+// export const deleteBookmark = ({ bookmarkId }) => {
+//   return instance.post(`api/bookmark/delete/${bookmarkId}`);
+// };
+
+/**
+ * 북마크 수정
+ * @param {number, string, string} param0
+ * @returns
+ */
+export const updateBookmark = ({ bookmarkId, bookmarkName, description }) => {
+  return instance.post(`/api/bookmark/update/${bookmarkId}`, {
+    bookmarkName: bookmarkName,
+    description: description,
+  });
+};
+
+/**
+ * 북마크 이동
+ * @param {array, number} param0
+ * @returns
+ */
+// export const moveBookmark = ({ bookmarkIdList, toCategoryId }) => {
+//   return instance.post("/api/bookmark/move", {
+//     bookmarkIdList: bookmarkIdList,
+//     toCategoryId: toCategoryId,
+//   });
+// };
+
+/**
+ * 북마크 조회
+ * @param {string, string} param0
+ * @returns
+ */
+// export const searchBookmark = ({ search = "", tag }) => {
+//   const query = `?search=${search}${tag && `tag=${tag}`}`;
+//   return instance.get("/api/bookmark/search" + query);
+// };
