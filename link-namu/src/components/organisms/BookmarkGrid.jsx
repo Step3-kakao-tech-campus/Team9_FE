1fd8548a2b815d496647188f3691f679248475e4
dabinchiii <dabinchi388@naver.com>
2023-10-12 15:29:58 +0900

    refactor: 카드 컴포넌트에 key 프로퍼티 추가

--- link-namu/src/components/organisms/BookmarkGrid.jsx
+++ link-namu/src/components/organisms/BookmarkGrid.jsx
@@ -5,11 +5,12 @@ const BookmarkGrid = ({ bookmarkList }) => {
 
   return (
     <>
-      <div className="grid grid-cols-4 gap-y-10 justify-items-center p-5">
+      <div className="grid grid-cols-3 gap-y-10 justify-items-center p-10">
         {bookmarkList &&
           bookmarkList.map((bookmark) => {
             return (
               <Card
+                key={bookmark.bookmarkId}
                 title={bookmark.title}
                 description={bookmark.description}
                 tags={bookmark.tags}
