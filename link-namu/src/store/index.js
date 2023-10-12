62894d345d4a7d0525ab767a3aa229a8f391833a
dabinchiii <dabinchi388@naver.com>
2023-10-12 15:23:59 +0900

    feat: 북마크 슬라이스 추가

--- link-namu/src/store/index.js
+++ link-namu/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
+import bookmarkReducer from "./slices/bookmarkSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    bookmark: bookmarkReducer,
   },
 });
 
