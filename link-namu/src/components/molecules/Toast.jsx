3d447a15d2062367c5b3f9e7ba93b6bfbdb65958
SeonghyeonKim <kkgojn@naver.com>
2023-10-15 18:16:00 +0900

    feat: Toast 컴포넌트에 type 추가

--- link-namu/src/components/molecules/Toast.jsx
+++ link-namu/src/components/molecules/Toast.jsx
@@ -1,24 +1,41 @@
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 /**
  * 메세지를 화면 중앙 하단에 출력하는 컴포넌트, 버튼과 메세지로 구성
  * @param {string} message - 토스트 메세지
  * @param {string} buttonName - 버튼 텍스트
  * @param {string} buttonStyle - tailwind CSS의 className 형식으로 작성
+ * @param {{error, success}} type - 토스트의 유형
  */
-const Toast = ({ message, buttonName, buttonStyle }) => {
-  const notify = () =>
-    toast.success(message, {
-      position: 'bottom-center',
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: 'light',
-    })
+const Toast = ({ message, buttonName, buttonStyle, type }) => {
+  var notify;
+
+  if (type === "success") {
+    notify = () =>
+      toast.success(message, {
+        position: "bottom-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+  } else if (type === "error") {
+    notify = () =>
+      toast.error(message, {
+        position: "bottom-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+  }
 
   return (
     <div>
@@ -38,7 +55,7 @@ const Toast = ({ message, buttonName, buttonStyle }) => {
         theme="light"
       />
     </div>
-  )
-}
+  );
+};
 
-export default Toast
+export default Toast;
