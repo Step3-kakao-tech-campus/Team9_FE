f7666fabe96530b8584c8a36bc3338d8082bf039
dabinchiii <dabinchi388@naver.com>
2023-10-15 19:47:23 +0900

    feat: 토큰 재발급하여 access token을 요청하고 store에 저장

--- link-namu/src/pages/MainPage.jsx
+++ link-namu/src/pages/MainPage.jsx
@@ -1,10 +1,35 @@
 import cookies from "react-cookies";
 import BookmarkGridTemplate from "../components/templates/BookmarkGridTemplate";
+import { useDispatch, useSelector } from "react-redux";
+import { reissue } from "../apis/user";
+import { setToken } from "../store/slices/userSlice";
 
 const MainPage = () => {
-  if (!cookies.load("accessToken")) {
-    window.location.href = "/signin";
-    return;
+  const dispatch = useDispatch();
+  const accessToken = useSelector((state) => {
+    return state.user.accessToken;
+  });
+
+  if (!accessToken) {
+    if (!cookies.load("refreshToken")) {
+      window.location.href = "/signin";
+      return;
+    }
+
+    reissue()
+      .then((res) => {
+        const accessToken = res.data?.response?.accessToken.split(" ")[1];
+        const refreshToken = res.data?.response?.refreshToken;
+
+        dispatch(
+          setToken({
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+          })
+        );
+        console.log("토큰이 재발급되었습니다.", accessToken);
+      })
+      .catch((err) => console.log(err));
   }
 
   return (
