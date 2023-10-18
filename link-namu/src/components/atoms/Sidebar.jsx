5bce39b0e6c3a827c583ad1b86ee2e3a13346000
dabinchiii <dabinchi388@naver.com>
2023-10-19 00:04:33 +0900

    feat: 카카오톡 가져오기 타일 클릭 시 모달창 열기 기능 추가

--- link-namu/src/components/atoms/Sidebar.jsx
+++ link-namu/src/components/atoms/Sidebar.jsx
@@ -4,12 +4,25 @@ import logoShare from "../../assets/share.png";
 import logoGoogle from "../../assets/google_logo.png";
 import logoKakao from "../../assets/kakaotalk_logo.png";
 import addBookmark from "../../assets/add_bookmark_with_link.png";
+import { useDispatch } from "react-redux";
+import { openModal } from "../../store/slices/modalSlice";
 
 /**
  * 우측 고정 위치의 사이드바
  * @returns
  */
 const Sidebar = () => {
+  const dispatch = useDispatch();
+
+  const handleOpenKakaoModal = () => {
+    dispatch(
+      openModal({
+        modalType: "KakaoModal",
+        isOpen: true,
+      })
+    );
+  };
+
   return (
     <>
       <div className="w-[60px] h-screen"></div>
@@ -18,7 +31,11 @@ const Sidebar = () => {
       >
         <SidebarTile src={logoShare} alt="share" />
         <SidebarTile src={logoGoogle} alt="google import " />
-        <SidebarTile src={logoKakao} alt="kakaotalk import " />
+        <SidebarTile
+          src={logoKakao}
+          alt="kakaotalk import "
+          onClick={handleOpenKakaoModal}
+        />
         <SidebarTile
           src={addBookmark}
           alt="add bookmark with link"
