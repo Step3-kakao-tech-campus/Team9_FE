7e44f9f32789bed7294264773cb48fbdb9eaad43
dabinchiii <dabinchi388@naver.com>
2023-10-18 18:06:31 +0900

    feat: store에 modal slice 추가

---
+++ link-namu/src/store/slices/modalSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  modalType: "",
+  isOpen: false,
+};
+
+const modalSlice = createSlice({
+  name: "modal",
+  initialState,
+  reducers: {
+    openModal: (state, action) => {
+      state.modalType = action.payload.modalType;
+      state.isOpen = true;
+    },
+    closeModal: (state) => {
+      state.isOpen = false;
+    },
+  },
+});
+
+export const { openModal, closeModal } = modalSlice.actions;
+export const selectModal = (state) => state.modal;
+
+export default modalSlice.reducer;
