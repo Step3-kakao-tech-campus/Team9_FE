386cf66a1aaa9ae42088e3340ac9fb73f62a2e6d
dabinchiii <dabinchi388@naver.com>
2023-10-27 00:01:24 +0900

    feat: store에 workspace slice 추가

--- link-namu/src/store/index.js
+++ link-namu/src/store/index.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
 import bookmarkReducer from "./slices/bookmarkSlice";
+import workspaceSlice from "./slices/workspaceSlice";
 import modalReducer from "./slices/modalSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     bookmark: bookmarkReducer,
+    workspace: workspaceSlice,
     modal: modalReducer,
   },
 });
