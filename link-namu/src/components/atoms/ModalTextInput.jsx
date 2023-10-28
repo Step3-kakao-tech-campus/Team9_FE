ae07431c68b7f89824754424bd0da2c9630f65d7
dabinchiii <dabinchi388@naver.com>
2023-10-29 03:41:25 +0900

    fix: 상위 컴포넌트와 값이 동기화되지 않는 문제 해결

--- link-namu/src/components/atoms/ModalTextInput.jsx
+++ link-namu/src/components/atoms/ModalTextInput.jsx
@@ -1,11 +1,23 @@
-const ModalTextInput = ({ id, placeholder, changeHandler }) => {
+import { useEffect, useState } from "react";
+
+const ModalTextInput = ({ id, value, placeholder, changeHandler }) => {
+  const [text, setText] = useState(value);
+
+  useEffect(() => {
+    setText(value);
+  }, [value]);
+
   return (
     <input
       type="text"
       id={id}
+      value={text}
       placeholder={placeholder}
-      onChange={changeHandler}
-      className="block w-full border p-4 text-base rounded-lg"
+      onChange={(e) => {
+        setText(e.target.value);
+        changeHandler(e.target.value);
+      }}
+      className="block w-full p-3 border border-[#56678942] rounded-lg text-base"
     />
   );
 };
