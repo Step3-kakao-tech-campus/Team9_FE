7e3b85ef488ef33e8ac39b9857a1dbde4a827512
SeonghyeonKim <kkgojn@naver.com>
2023-11-01 17:14:09 +0900

    feat: 드래그 속성 추가

--- link-namu/src/components/molecules/Card.jsx
+++ link-namu/src/components/molecules/Card.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Draggable } from "react-beautiful-dnd";
 import Tag from "../atoms/Tag";
 
 /**
@@ -9,43 +11,59 @@ import Tag from "../atoms/Tag";
  * @param {array<string>} tags - 태그 명 배열
  */
 const Card = ({
+  index = 0,
+  dragId = "",
+  bookmarkId = "",
   imageUrl = "",
   imageAlt = "",
   title = "",
   description = "",
   tags = [],
 }) => {
+  const [isDragging, setIsDragging] = useState(false);
+
   return (
-    <div className="transform bg-white border-2 rounded-md shadow-md w-72 h-80 hover:transition-transform hover:scale-105 hover:border hover:border-gray-300 hover:shadow-lg">
-      {/* 이미지 영역 */}
-      <img
-        src={imageUrl}
-        alt={imageAlt}
-        className="object-cover w-64 h-40 mx-auto my-1 rounded-sm"
-      />
+    <Draggable key={bookmarkId} draggableId={dragId} index={1}>
+      {provided => (
+        <div
+          className={`transform bg-white border-2 rounded-md shadow-md w-72 h-80 hover:transition-transform hover:scale-105 hover:border hover:border-gray-300 hover:shadow-lg`}
+          onMouseEnter={() => setIsDragging(true)}
+          onMouseLeave={() => setIsDragging(false)}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {/* 이미지 영역 */}
+          <img
+            src={imageUrl}
+            alt={imageAlt}
+            className="object-cover w-64 h-40 mx-auto my-1 rounded-sm"
+          />
 
-      {/* 내용 영역 */}
-      <div className="px-4 py-4">
-        <div className="mb-2 overflow-hidden text-xl font-bold text-ellipsis">
-          {title}
+          {/* 내용 영역 */}
+          <div className="px-4 py-4">
+            <div className="mb-2 overflow-hidden text-xl font-bold text-ellipsis">
+              {title}
+            </div>
+            <p className="overflow-hidden text-base text-gray-700 text-ellipsis">
+              {description}
+            </p>
+          </div>
+
+          {/* 꼬리 영역 */}
+          <div className="px-2 py-2">
+            {/* 태그 영역 */}
+            <span className="inline-block w-[80%]">
+              {tags.map((tag, index) => (
+                <Tag key={index} name={tag.tagName} />
+              ))}
+            </span>
+            {/* 버튼 영역 */}
+            <span className="inline-block w-[20%]"></span>
+          </div>
         </div>
-        <p className="overflow-hidden text-base text-gray-700 text-ellipsis">
-          {description}
-        </p>
-      </div>
-
-      {/* 꼬리 영역 */}
-      <div className="px-2 py-2">
-        {/* 태그 영역 */}
-        <span className="inline-block w-[80%]">
-          {tags.map((tag, index) => (
-            <Tag key={index} name={tag.tagName} />
-          ))}
-        </span>
-        {/* 버튼 영역 */}
-        <span className="inline-block w-[20%]"></span>
-      </div>
-    </div>
+      )}
+    </Draggable>
   );
 };
 
