f4ab39bfb9c251cc2d3a97224749054cda644aa3
dabinchiii <dabinchi388@naver.com>
2023-11-03 23:55:00 +0900

    feat(Modal): 카카오 모달에 사용할 아이템 컴포넌트 추가
    - 북마크의 이미지, 제목, 링크 정보를 받아 나타냄
    - 카테고리 select box를 포함
    - 개별 아이템을 선택가능하도록 체크박스 포함

---
+++ link-namu/src/components/atoms/BookmarkSelectItem.jsx
@@ -0,0 +1,62 @@
+import WorkspaceSeleceBox from "./WorkspaceSelectBox";
+import CategorySelectBox from "./CategorySelectBox";
+import { useEffect, useState } from "react";
+import Checkbox from "./Checkbox";
+
+const BookmarkSelectItem = ({
+  id,
+  checked = false,
+  handleCheckedChange,
+  thumbnail = null,
+  title = "북마크 제목",
+  url = "",
+}) => {
+  const [workspaceId, setWorkspaceId] = useState(null);
+  const [categoryId, setCategoryId] = useState(null);
+
+  return (
+    <div
+      className={`grow flex flex-row gap-x-4 p-2 mb-1 border rounded-xl ${
+        checked ? "bg-[#ecf8fc]" : "bg-[#ffffff]"
+      }`}
+    >
+      <img
+        src={thumbnail}
+        alt="thumbnail"
+        className="w-[50px] h-[50px] border"
+      />
+      <div className="grow">
+        <input
+          className="block w-full border"
+          defaultValue={title}
+          disabled={`${!checked ? "disabled" : ""}`}
+        />
+        <input readOnly defaultValue={url} className="w-full border-b" />
+      </div>
+      <div className="w-[200px]">
+        <WorkspaceSeleceBox
+          value={workspaceId}
+          changeHandler={setWorkspaceId}
+          isSlimType={true}
+          disabled={!checked}
+        />
+        <CategorySelectBox
+          workspaceId={workspaceId}
+          value={categoryId}
+          changeHandler={setCategoryId}
+          isSlimType={true}
+          disabled={!checked}
+        />
+      </div>
+      <input
+        id={id}
+        checked={checked}
+        onChange={handleCheckedChange}
+        type="checkbox"
+      />
+      {/* <Checkbox id={id} checked={checked} onChange={handleCheckedChange} /> */}
+    </div>
+  );
+};
+
+export default BookmarkSelectItem;
