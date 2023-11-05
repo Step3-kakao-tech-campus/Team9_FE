32c5d9e6d906c7caccf6d71c2d3930e33fb9a392
dabinchiii <dabinchi388@naver.com>
2023-11-06 06:25:04 +0900

    style(Menubar): ContextMenuItem의 스타일 변경
    - 요소를 div에서 button으로 변경
    - 글자 크기 변경

--- link-namu/src/components/atoms/ContextMenuItem.jsx
+++ link-namu/src/components/atoms/ContextMenuItem.jsx
@@ -1,11 +1,11 @@
 const ContextMenuItem = ({ handleAction, children }) => {
   return (
-    <div
-      className="context-menu-item py-1 px-3 hover:bg-[#f6f6f6]"
+    <button
+      className="context-menu-item block w-full text-left py-1 px-3 text-sm hover:bg-[#f6f6f6]"
       onClick={() => handleAction()}
     >
       {children}
-    </div>
+    </button>
   );
 };
 
