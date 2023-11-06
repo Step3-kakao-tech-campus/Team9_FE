6a5327ee5ce630bd83b2841664ae4b7dfee83fca
dabinchiii <dabinchi388@naver.com>
2023-11-06 21:42:28 +0900

    feat(Modal): 공유 링크에서 id와 type 추출하고 api 연결하는 기능 구현
    - 입력된 공유 링크에서 type(category or workspace)와 암호화된 ID를
      추출하는 기능 구현
    - type에 따라 해당하는 API 호출 함수에 연결

--- link-namu/src/components/organisms/SaveShareLinkModal.jsx
+++ link-namu/src/components/organisms/SaveShareLinkModal.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
+import { useCloseModal } from "../../hooks/useCloseModal";
 import {
-  getWorkspaceFromEncodedId,
-  getCategoryFromEncodedId,
+  addWorkspaceFromEncodedId,
+  addCategoryFromEncodedId,
 } from "../../apis/share";
 
 import SingleStepModalBase from "../organisms/SingleStepModalBase";
@@ -11,27 +12,82 @@ import ModalSubTitle from "../atoms/ModalSubtitle";
 import ModalTextInput from "../atoms/ModalTextInput";
 
 const SaveShareLinkModal = () => {
+  const closeModal = useCloseModal();
   const [shareLink, setShareLink] = useState(null);
+  const LINK_TYPE = {
+    WORKSPACE: "workspace",
+    CATEGORY: "category",
+  };
 
   useEffect(() => {
     console.log(shareLink);
   }, [shareLink]);
 
   const extractCode = (url) => {
-    const result = {};
-    const query = url.split("?")[1];
+    const query = url?.split("?")[1];
+    const [type, encodedId] = query?.split("=");
+
+    return {
+      type: type,
+      encodedId: encodedId + "=",
+    };
   };
 
   const saveShareLink = () => {
-    const data = extractCode(shareLink);
+    try {
+      if (!shareLink) {
+        throw new Error("공유 링크를 입력해주세요.");
+      }
 
-    if (data.type === "category") {
-      getCategoryFromEncodedId();
+      const data = extractCode(shareLink);
+      if (!data.type || !data.encodedId) {
+        throw new Error("링크 분석 중 오류가 발생하였습니다.");
+      }
+      console.log("type: ", data.type);
+      console.log("encodedId: ", data.encodedId);
+
+      if (data.type === LINK_TYPE.CATEGORY) {
+        addCategoryFromEncodedId({
+          encodedCategoryId: data.encodedId,
+        }).then((res) => {
+          console.log(res);
+
+          if (res?.status !== 200) {
+            throw new Error(res); // 에러 메시지로 수정하기
+          }
+
+          alert("추가되었습니다.");
+          closeModal();
+        });
+      } else if (data.type === LINK_TYPE.WORKSPACE) {
+        addWorkspaceFromEncodedId({ encodedWorkspaceId: data.encodedId }).then(
+          (res) => {
+            console.log(res);
+
+            if (res?.statue !== 200) {
+              throw new Error(res); // 에러 메시지로 수정하기
+            }
+
+            alert("추가되었습니다.");
+            closeModal();
+          }
+        );
+      } else {
+        throw new Error("링크 분석 중 오류가 발생하였습니다.");
+      }
+    } catch (err) {
+      const msg = "[공유 링크 가져오기] " + err.message;
+      console.log(msg);
+      alert(msg);
     }
   };
 
   return (
-    <SingleStepModalBase size="md" clickHandler={saveShareLink}>
+    <SingleStepModalBase
+      size="md"
+      buttonName="가져오기"
+      clickHandler={saveShareLink}
+    >
       <ModalTitle>공유 링크로 가져오기</ModalTitle>
       <ModalBox>
         <div>
