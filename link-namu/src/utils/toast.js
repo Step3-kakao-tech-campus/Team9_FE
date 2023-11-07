84243131b2feca21d872c0abbd81fce0d78a0b53
SeonghyeonKim <kkgojn@naver.com>
2023-11-07 17:34:59 +0900

    docs: 주석 수정

--- link-namu/src/utils/toast.js
+++ link-namu/src/utils/toast.js
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 /**
  * 토스트 메세지를 출력하는 함수
  * @param {string} message
- * @param {{default, success, error, warning, info}} type
+ * @param {[default, success, error, warning, info]} type
  * @returns 토스트 메세지 출력
  */
 export const printToast = (message, type = "default") => {
