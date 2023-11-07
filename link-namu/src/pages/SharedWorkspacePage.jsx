4e9b8fcfbae47d3667b7579b5c6fc35174bce31e
dabinchiii <dabinchi388@naver.com>
2023-11-07 14:38:09 +0900

    feat(page): 공유된 워크스페이스를 표시하는 페이지에서 카테고리 목록 표시 기능
    - 페이지 url에서 암호화된 워크스페이스 ID를 추출
    - api 요청을 위해 토큰을 재발급
    - 추출된 ID로 워크스페이스 이름과 카테고리 목록 데이터를 가져옴
    - 클릭하면 해당 카테고리 페이지로 이동하는 아이템 추가
    - 디자인 변경 필요

--- link-namu/src/pages/SharedWorkspacePage.jsx
+++ link-namu/src/pages/SharedWorkspacePage.jsx
@@ -1,7 +1,72 @@
+import { useEffect, useState } from "react";
+import { getWorkspaceFromEncodedId } from "../apis/share";
+import { useReissueToken } from "../hooks/useReissueToken";
+
 const SharedWorkspacePage = () => {
+  const reissueToken = useReissueToken();
+  const [isTokenReady, setIsTokenReady] = useState(false);
+  const [encodedId, setEncodedId] = useState(null);
+  const [workspaceName, setWorkspaceName] = useState("");
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    reissueToken({
+      changeState: () => setIsTokenReady(true),
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!isTokenReady) return;
+    const currentUrl = window.location.href;
+    const query = currentUrl?.split("?")[1];
+    setEncodedId(query.split("=")[1] + "=");
+  }, [isTokenReady]);
+
+  useEffect(() => {
+    if (!encodedId) return;
+
+    getWorkspaceFromEncodedId({ encodedWorkspaceId: encodedId }).then((res) => {
+      console.log("res", res);
+
+      const data = res?.data?.response;
+      setWorkspaceName(data.workSpaceName);
+      setCategoryList(data.sharedCategoryList);
+    });
+  }, [encodedId]);
+
+  const getCategoryLink = ({ id }) => {
+    return window.location.origin + "/share-link/category/share?category=" + id;
+  };
   return (
     <div>
       <h1>공유된 워크스페이스를 표시하는 페이지</h1>
+      <span>{encodedId}</span>
+      <div>
+        <span>워크스페이스 이름 : </span>
+        <span>{workspaceName}</span>
+      </div>
+      <div>
+        <div className="grid grid-cols-5 gap-x-10 gap-y-10">
+          {categoryList.map((category) => {
+            return (
+              <div className="">
+                <a
+                  href={
+                    window.location.origin +
+                    "/share-link/category/share?category=" +
+                    category.shareCategoryLink
+                  }
+                >
+                  <div className="h-[200px] border">
+                    <h5>{category.categoryName}</h5>
+                    {category.shareCategoryLink}
+                  </div>
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
