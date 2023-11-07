1a6565ff497ed3bd0d87af5f1699ae92b733244c
dabinchiii <dabinchi388@naver.com>
2023-11-07 21:25:36 +0900

    feat(Modal): 구글 docs 연동 모달 기능 구현
    - 구글 문서 페이지 링크에서 document ID 추출 기능
    - document ID로 구글 문서 페이지 연동 기능 구현

--- link-namu/src/components/organisms/GoogleDocsModal.jsx
+++ link-namu/src/components/organisms/GoogleDocsModal.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { registerGoogleDocs } from "../../apis/googleDocs";
+import { printToast } from "../../utils/toast";
+
 import SingleStepModalBase from "./SingleStepModalBase";
 import ModalTitle from "../atoms/ModalTitle";
 import ModalBox from "../atoms/ModalBox";
@@ -5,13 +9,54 @@ import ModalSubTitle from "../atoms/ModalSubtitle";
 import ModalTextInput from "../atoms/ModalTextInput";
 
 const GoogleDocsModal = () => {
+  const [pageLink, setPageLink] = useState(null);
+
+  const extractDocumentId = (url) => {
+    if (!url) return "";
+
+    const documentIdPart = url.replace(
+      "https://docs.google.com/document/d/",
+      ""
+    );
+    const documentId = documentIdPart.split("/")[0];
+    console.log();
+    return documentId;
+  };
+
+  const handleGoogleDocsRegistration = () => {
+    if (!pageLink) {
+      printToast("구글 문서 페이지 URL을 입력해주세요.");
+      return;
+    }
+
+    const documentId = extractDocumentId(pageLink);
+    console.log("document id : ", documentId);
+
+    registerGoogleDocs({ documentId: documentId })
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error(res?.data?.error?.message);
+        }
+        console.log("google docs res", res);
+      })
+      .catch((err) => {
+        const msg = "[구글 DOCS 연동] " + err.message;
+        console.log(msg);
+        printToast(msg, "error");
+      });
+  };
+
   return (
-    <SingleStepModalBase size="md" buttonName="등록하기">
+    <SingleStepModalBase
+      size="md"
+      buttonName="등록하기"
+      clickHandler={handleGoogleDocsRegistration}
+    >
       <ModalTitle>구글 문서 페이지 등록하기</ModalTitle>
       <ModalBox>
         <div>
-          <ModalSubTitle>구글 문서 페이지 ID</ModalSubTitle>
-          <ModalTextInput />
+          <ModalSubTitle>구글 문서 페이지 URL</ModalSubTitle>
+          <ModalTextInput value={pageLink} changeHandler={setPageLink} />
         </div>
       </ModalBox>
     </SingleStepModalBase>
