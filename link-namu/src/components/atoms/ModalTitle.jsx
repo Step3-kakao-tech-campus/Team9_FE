eddd5a0d899f9c95dd8b263abf41fadcebaf3d37
dabinchiii <dabinchi388@naver.com>
2023-11-08 17:00:53 +0900

    style(Modal): title 컴포넌트와 subtitle 컴포넌트 스타일 변경

--- link-namu/src/components/atoms/ModalTitle.jsx
+++ link-namu/src/components/atoms/ModalTitle.jsx
@@ -1,7 +1,7 @@
 const ModalTitle = ({ children }) => {
   return (
     <div className="relative px-6 py-4 text-left">
-      <span className="text-xl font-bold">{children}</span>
+      <h2 className="text-xl text-center font-semibold">{children}</h2>
     </div>
   );
 };
