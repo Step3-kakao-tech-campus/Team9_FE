f4324c0fb5270b431563d25e789ec7c0ffd66091
dabinchiii <dabinchi388@naver.com>
2023-11-09 18:07:00 +0900

    refactor(store): state.workspace.workspaceList를 가져오는 함수 변경
    - state.workspace를 반환하는 함수를 제거하고 state.workspace.workspaceList를
      반환하는 함수 추가 및 적용

--- link-namu/src/hooks/useWorkspaceList.js
+++
@@ -1,7 +0,0 @@
-import { useSelector } from "react-redux";
-import { selectWorkspaceList } from "../store/slices/workspaceSlice";
-
-export function useWorkspaceList() {
-  const workspaceList = useSelector(selectWorkspaceList);
-  return workspaceList;
-}
