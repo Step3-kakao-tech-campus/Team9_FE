c669436e3660030da667b38552836136dfdc3bcb
dabinchiii <dabinchi388@naver.com>
2023-11-09 19:26:14 +0900

    fix(hook): 워크스페이스 리스트를 가져오는 커스텀 훅 추가

---
+++ link-namu/src/hooks/useWorkspaceList.js
@@ -0,0 +1,52 @@
+import { useQuery } from "react-query";
+import { useDispatch, useSelector } from "react-redux";
+import { setWorkspaceList } from "../store/slices/workspaceSlice";
+import { getWorkspaceList } from "../apis/workspace";
+import { getAccessToken } from "../store";
+import { useEffect } from "react";
+
+const useWorkspaceList = () => {
+  const dispatch = useDispatch();
+  const accessToken = useSelector(getAccessToken);
+
+  const {
+    data: workspaceData,
+    isLoading,
+    isError,
+    refetch,
+  } = useQuery("workspaceList", getWorkspaceList);
+
+  const refetchData = async () => {
+    await refetch();
+  };
+
+  useEffect(() => {
+    if (accessToken) {
+      refetchData();
+    }
+  }, [accessToken]);
+
+  if (isLoading) {
+    console.log("워크스페이스 리스트 로딩 중");
+    return { isLoading: true };
+  }
+
+  // 데이터 로딩 중 에러가 발생한 경우
+  if (isError) {
+    console.log("워크스페이스 리스트 로딩 에러");
+    return { isError: true };
+  }
+
+  if (workspaceData) {
+    console.log("workspaceData", workspaceData);
+    if (workspaceData.status === 200) {
+      dispatch(
+        setWorkspaceList({ workspaceList: workspaceData.data?.response })
+      );
+    }
+  }
+
+  return { workspaceData, refetchData, isLoading, isError };
+};
+
+export { useWorkspaceList };
