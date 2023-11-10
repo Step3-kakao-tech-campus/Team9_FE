2ac1e99b03ce34cfc7062d0fd7d8815d397986cc
dabinchiii <dabinchi388@naver.com>
2023-11-10 18:39:11 +0900

    feat(Searchbar): 검색창에 입력 후 엔터키 누르면 검색 버튼 클릭되는 기능

--- link-namu/src/components/atoms/Searchbar.jsx
+++ link-namu/src/components/atoms/Searchbar.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
+import DetailSearchBox from "./DetailSearchBox";
 import magnifier from "../../assets/magnifier.png";
 import xIcon from "../../assets/x.png";
-import { useNavigate } from "react-router-dom";
+
 /**
  * 검색창 컴포넌트
  * @param {string} width - 컴포넌트 가로 길이
@@ -11,6 +13,7 @@ import { useNavigate } from "react-router-dom";
 const Searchbar = () => {
   const navigate = useNavigate();
   const inputRef = useRef(null);
+  const searchButtonRef = useRef(null);
   const [isFocused, setIsFocus] = useState(false);
   const [isExtended, setIsExtended] = useState(false);
   const [text, setText] = useState("");
@@ -23,70 +26,77 @@ const Searchbar = () => {
     setText("");
     inputRef.current.focus();
   };
+  const handleEnterKeyPress = (e) => {
+    if (e.key === "Enter") {
+      searchButtonRef.current.click();
+    }
+  };
 
   const searchText = () => {
     navigate(`/search/result`, {
       state: {
         bookmarkName: text,
-        bookmarkLink: text,
-        bookmarkDescription: text,
-        workspaceName: text,
-        tags: null,
       },
     });
   };
   return (
-    <div className="flex gap-x-2">
-      <div
-        className={`w-[550px] h-[40px] pl-5 py-[10px] flex items-center border rounded-[50px] shadow-md ${
-          isFocused && "shadow-[#729076]"
-        }`}
-      >
-        <div>
-          <img
-            src={magnifier}
-            alt="magnifier icon"
-            className="w-[20px] h-[20px] "
-          />
-        </div>
-        <input
-          ref={inputRef}
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          onFocus={() => {
-            setIsFocus(true);
-          }}
-          onBlur={() => {
-            setIsFocus(false);
-          }}
-          className={`block flex-grow ml-5 outline-none`}
-        />
-        {text.length > 0 && (
-          <button
-            className="block h-6 w-6 m-2 flex items-center justify-center rounded-full hover:bg-[#eeeeee]"
-            onClick={clearText}
-          >
-            <div className="w-3 h-3">
-              <img
-                src={xIcon}
-                alt="clear search text button"
-                className="block"
-              />
-            </div>
-          </button>
-        )}
-        <button
-          className={`block h-[40px] px-5 border rounded-tr-full rounded-br-full`}
-          onClick={searchText}
+    <>
+      <div className="flex gap-x-2">
+        <div
+          className={`w-[550px] h-[40px] pl-5 py-[10px] flex items-center border rounded-[50px] shadow-md ${
+            isFocused && "shadow-[#729076]"
+          }`}
         >
-          <span className="block text-sm">검색</span>
+          <div>
+            <img
+              src={magnifier}
+              alt="magnifier icon"
+              className="w-[20px] h-[20px] "
+            />
+          </div>
+          <input
+            ref={inputRef}
+            type="text"
+            value={text}
+            placeholder="북마크 제목에서 검색"
+            onChange={(e) => setText(e.target.value)}
+            onFocus={() => {
+              setIsFocus(true);
+            }}
+            onBlur={() => {
+              setIsFocus(false);
+            }}
+            onKeyPress={handleEnterKeyPress}
+            className={`block flex-grow ml-5 outline-none`}
+          />
+          {text.length > 0 && (
+            <button
+              className="block h-6 w-6 m-2 flex items-center justify-center rounded-full hover:bg-[#eeeeee]"
+              onClick={clearText}
+            >
+              <div className="w-3 h-3">
+                <img
+                  src={xIcon}
+                  alt="clear search text button"
+                  className="block"
+                />
+              </div>
+            </button>
+          )}
+          <button
+            ref={searchButtonRef}
+            className={`block h-[40px] px-5 border rounded-tr-full rounded-br-full`}
+            onClick={searchText}
+          >
+            <span className="block text-sm">검색</span>
+          </button>
+        </div>
+        <button className={`${!isExtended && "invisible"} px-2`}>
+          <span className="text-sm">상세 검색</span>
         </button>
       </div>
-      <button className={`${!isExtended && "invisible"} px-2`}>
-        <span className="text-sm">상세 검색</span>
-      </button>
-    </div>
+      {/* <DetailSearchBox /> */}
+    </>
   );
 };
 
