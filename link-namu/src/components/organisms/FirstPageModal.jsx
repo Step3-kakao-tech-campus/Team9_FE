7e7eb8350fc273f3cd82ebe99c6c921ad71e0e00
SeonghyeonKim <kkgojn@naver.com>
2023-11-11 03:05:57 +0900

    feat: 시작페이지 마지막 모달 버튼 추가

--- link-namu/src/components/organisms/FirstPageModal.jsx
+++ link-namu/src/components/organisms/FirstPageModal.jsx
@@ -1,4 +1,12 @@
-const FirstPageModal = ({ index, count, children, isEnd = false }) => {
+import LoginButton from "../atoms/LoginButton";
+
+const FirstPageModal = ({
+  index,
+  count,
+  children,
+  isEnd = false,
+  startHandler,
+}) => {
   return (
     <div
       className={`max-w-lg p-8 m-2 bg-white rounded-md shadow-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${
@@ -13,6 +21,12 @@ const FirstPageModal = ({ index, count, children, isEnd = false }) => {
         <h1 className="mb-4 text-xl font-bold">{children}</h1>
         {!isEnd && (
           <div className="text-gray-600 delay-1000">클릭해주세요.</div>
+        )}
+        {isEnd && (
+          <div className="flex justify-between w-full mt-2">
+            <button onClick={startHandler}>처음으로</button>
+            <LoginButton />
+          </div>
         )}
       </div>
     </div>
