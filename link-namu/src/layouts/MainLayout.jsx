00cc74ac65b5e735ff7950c4706a5d02b1a18824
SeonghyeonKim <kkgojn@naver.com>
2023-11-11 20:43:16 +0900

    fix: 엑세스 토큰이 없는 경우 수정

--- link-namu/src/layouts/MainLayout.jsx
+++ link-namu/src/layouts/MainLayout.jsx
@@ -6,31 +6,51 @@ import Sidebar from "../components/atoms/Sidebar";
 import GNB from "../components/atoms/GNB";
 import Menubar from "../components/molecules/Menubar";
 import Loader from "../components/atoms/Loader";
+import { getAccessToken } from "../store";
+import DefaultGNB from "../components/atoms/DefaultGNB";
+import LoginButton from "../components/atoms/LoginButton";
 
 const MainLayout = () => {
   const [isOpen, setIsOpen] = useState(true);
+  const accessToken = getAccessToken();
 
   return (
     <>
       <div className="w-full h-[56px]"></div>
       <div className="fixed top-[56px] left-0 right-0 bottom-0 flex mr-[60px] overflow-y-hidden">
         <div className={`${isOpen ? "w-[256px]" : "w-0"} duration-500`}>
-          <Menubar isOpen={isOpen} />
+          {accessToken && <Menubar isOpen={isOpen} />}
         </div>
         <div className="flex-1 h-[100%]">
           <Suspense fallback={<Loader />}>
-            <Scrollbars thumbSize={100}>
+            {accessToken ? (
+              <Scrollbars thumbSize={100}>
+                <Outlet />
+              </Scrollbars>
+            ) : (
               <Outlet />
-            </Scrollbars>
+            )}
           </Suspense>
         </div>
       </div>
-      <GNB
-        setState={() => {
-          setIsOpen((prev) => !prev);
-        }}
-      />
-      <Sidebar />
+      {accessToken ? (
+        <>
+          <GNB
+            setState={() => {
+              setIsOpen(prev => !prev);
+            }}
+          />
+          <Sidebar />
+        </>
+      ) : (
+        <>
+          <DefaultGNB>
+            <div className="fixed right-10">
+              <LoginButton />
+            </div>
+          </DefaultGNB>
+        </>
+      )}
     </>
   );
 };
