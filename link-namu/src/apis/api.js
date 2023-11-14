3c36147ad57fad9a0a8124e667bc16c315b3f4c4
dabinchiii <dabinchi388@naver.com>
2023-11-14 19:06:26 +0900

    feat: prettier

--- link-namu/src/apis/api.js
+++ link-namu/src/apis/api.js
@@ -19,13 +19,16 @@ instance.interceptors.request.use((config) => {
 
 // middleware
 instance.interceptors.response.use(
-  response => {
+  (response) => {
     return response;
   },
-  error => {
+  (error) => {
     console.log("error", error);
-    const status = error?.response?.status;
+    const status = error.response.status;
 
+    if (status === 401) {
+      // window.location.href = "/signin";
+    }
     if (status >= 500) {
       printToast("오류가 발생했습니다. 잠시 후 다시 시도해주세요", "error");
     }
