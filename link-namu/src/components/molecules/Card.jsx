8f7515dd4cc15403f3044110dfa3ddc6b375e787
dabinchiii <dabinchi388@naver.com>
2023-11-15 03:00:00 +0900

    feat: title 추가

--- link-namu/src/components/molecules/Card.jsx
+++ link-namu/src/components/molecules/Card.jsx
@@ -79,6 +79,7 @@ const Card = ({
       <Draggable key={bookmarkId} draggableId={dragId} index={1}>
         {(provided) => (
           <div
+            title={url}
             className={`transform bg-white border-2 rounded-md shadow-md w-72 h-80 
               ${
                 isHover &&
@@ -102,6 +103,7 @@ const Card = ({
             {/* 내용 영역 */}
             <div className="px-4 py-2">
               <div
+                title={title}
                 className={`mb-2 overflow-hidden font-bold break-all text-md text-ellipsis whitespace-nowrap ${
                   isHover && `overflow-normal text-clip`
                 }`}
@@ -110,6 +112,7 @@ const Card = ({
               </div>
               <hr />
               <p
+                title={description}
                 className={`mt-2 overflow-hidden text-sm text-gray-700 break-all whitespace-normal text-ellipsis line-clamp-2 
               ${isHover && `overflow-normal line-clamp-none text-clip`}`}
               >
