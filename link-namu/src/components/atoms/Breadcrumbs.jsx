8c9cbc550bb821ae62e6375e3bb5555eb8f57db6
dabinchiii <dabinchi388@naver.com>
2023-11-15 03:15:01 +0900

    feat: breadcrumbs의 max width 설정

--- link-namu/src/components/atoms/Breadcrumbs.jsx
+++ link-namu/src/components/atoms/Breadcrumbs.jsx
@@ -3,13 +3,20 @@ const Breadcrumbs = ({ workspaceName = "", categoryName = "" }) => {
   return (
     <div className="inline-block p-2 my-3 border rounded-lg">
       <div className="flex items-center text-sm justify-left gap-x-2 font-medium">
-        <span className={`text-[#5c5e64] text-[10px] leading-3 pl-3`}>
+        <span
+          title="MAIN"
+          className={`text-[#5c5e64] text-[10px] leading-3 pl-3`}
+        >
           MAIN
         </span>
         {arrow}
-        <span>{workspaceName}</span>
+        <span title={workspaceName} className="max-w-[300px] truncate">
+          {workspaceName}
+        </span>
         {arrow}
-        <span>{categoryName}</span>
+        <span title={categoryName} className="max-w-[300px] truncate">
+          {categoryName}
+        </span>
       </div>
     </div>
   );
